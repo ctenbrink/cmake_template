4b7b0e4f6c63cf4a779d85bad5e4f8c071e7f447
Chris Tenbrink <chris@chiefarchitect.com>
2013-05-17 11:36:00 -0700

    Updated rakefile to include Windows builds.

--- rakefile.rb
+++ rakefile.rb
@@ -3,6 +3,7 @@ require 'rake/clean'
 require 'fileutils'
 
 $project_name = "enter_a_project_name_here"
+$msbuildExe = 'C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe'
 
 CLOBBER.include('bin/**')
 CLOBBER.include('bin')
@@ -10,6 +11,8 @@ CLOBBER.include('bin')
 def platform
   if RUBY_PLATFORM =~ /darwin/
     platform = :osx
+  elsif RUBY_PLATFORM =~ /i386-mingw32/
+    platform = :win32
   else
     raise 'Unhandled platform'
   end
@@ -18,18 +21,21 @@ end
 def binary_directory
   case platform
   when :osx then 'bin/osx'
+  when :win32 then 'bin/win32'
   end
 end
 
 def command_line_for_build
   case platform
   when :osx then "xcodebuild -project #{$project_name}.xcodeproj"
+  when :win32 then "\"#{$msbuildExe}\" #{$project_name}.sln /target:Build /verbosity:detailed"
   end
 end
 
 def cmake_generator
   case platform
   when :osx then 'Xcode'
+  when :win32 then "\"Visual Studio 10\""
   end
 end
 
